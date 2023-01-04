6430428732d8ca3829312b5daa6932e15e826ca6
Esi Ogagaoghene <30408871+ogagaoghene@users.noreply.github.com>
2023-01-04 19:24:46 +0100

    Delete 10-update_uniq_items.js

--- 10-update_uniq_items.js
+++
@@ -1,5 +0,0 @@
-export default function updateUniqueItems(map) {
-  if (!(map instanceof Map)) throw Error('Cannot process');
-  for (const i of map) if (i[1] === 1) map.set(i[0], 100);
-  return map;
-}
